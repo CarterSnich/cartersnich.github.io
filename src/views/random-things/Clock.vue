6116010988e45a2c168822bf0ed818283d956808
CarterSnich <jmeclosa@proton.me>
2025-11-23 22:08:57 +0800

    code refactor

--- src/views/random-things/Clock.vue
+++ src/views/random-things/Clock.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import Modal from '@/components/Modal.vue';
 import { getTh } from '@/lib/utils';
 import { useDark, useToggle } from '@vueuse/core';
 import { computed, onMounted, ref } from 'vue';
 
 const toggleDark = useToggle(useDark());
-
 
 const isToolbarHidden = ref(false)
 const is24Hour = ref(true);
@@ -29,7 +27,50 @@ const dateDisplay = computed(() => {
   })
 })
 
-const isSettingsOpen = ref(false)
+const extraInfo = (() => {
+  const d = new Date();
+  const year = d.getFullYear();
+  const month = d.getMonth()
+  const date = d.getDate()
+  const time = d.getTime()
+
+  // timezone offset
+  const m = d.getTimezoneOffset();
+  const sign = m <= 0 ? "+" : "-";
+  const tzOffset = `${sign}${String(Math.floor(Math.abs(m) / 60)).padStart(2, "0")}:${String(Math.abs(m) % 60).padStart(2, "0")}`;
+
+  // timezone name
+  const tzName = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  // day of year
+  const dayOfYear = Math.floor((time - new Date(year, 0, 0).getTime()) / 86400000);
+
+  // ISO week number
+  const temp = new Date(year, month, date);
+  const dayNum = (temp.getDay() + 6) % 7;
+  temp.setDate(temp.getDate() - dayNum + 3);
+  const week1 = new Date(temp.getFullYear(), 0, 4);
+  const week = 1 + Math.round(((temp.getTime() - week1.getTime()) / 86400000 - 3) / 7);
+
+  // quarter of the year
+  const quarterOfYear = Math.floor((month + 3) / 3)
+
+  // leep year?
+  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+  // get century
+  const century = Math.floor((year - 1) / 100) + 1
+
+  return {
+    tzName,
+    tzOffset,
+    dayOfYear,
+    week,
+    quarterOfYear,
+    isLeapYear,
+    century
+  }
+})();
 
 function onPressKeybinds(e: KeyboardEvent) {
   switch (e.key.toLowerCase()) {
@@ -63,52 +104,6 @@ function onPressKeybinds(e: KeyboardEvent) {
   }
 }
 
-const extraInfo = (() => {
-  const d = new Date();
-  const year = d.getFullYear();
-  const month = d.getMonth()
-  const date = d.getDate()
-  const time = d.getTime()
-
-  // timezone offset
-  const m = d.getTimezoneOffset();
-  const sign = m <= 0 ? "+" : "-";
-  const tzOffset = `${sign}${String(Math.floor(Math.abs(m) / 60)).padStart(2, "0")}${String(Math.abs(m) % 60).padStart(2, "0")}`;
-
-  // timezone name
-  const tzName = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-  // day of year
-  const dayOfYear = Math.floor((time - new Date(year, 0, 0).getTime()) / 86400000);
-
-  // ISO week number
-  const temp = new Date(year, month, date);
-  const dayNum = (temp.getDay() + 6) % 7;
-  temp.setDate(temp.getDate() - dayNum + 3);
-  const week1 = new Date(temp.getFullYear(), 0, 4);
-  const week = 1 + Math.round(((temp.getTime() - week1.getTime()) / 86400000 - 3) / 7);
-
-  // quarter of the year
-  const quarterOfYear = Math.floor((month + 3) / 3)
-
-  // leep year?
-  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-
-  // get century
-  const century = Math.floor((year - 1) / 100) + 1
-
-  return {
-    tzOffset,
-    tzName,
-    dayOfYear,
-    week,
-    quarterOfYear,
-    isLeapYear,
-    century
-  }
-})();
-
-
 onMounted(() => {
   setInterval(() => {
     now.value = new Date();
@@ -122,7 +117,7 @@ onMounted(() => {
     <div id="time-display">{{ timeDisplay }}</div>
     <div id="date-display">{{ dateDisplay }}</div>
     <small id="extra-info" v-if="!isExtraInfoHidden">
-      <span>UTC{{ extraInfo.tzOffset }} ({{ extraInfo.tzName }})</span>
+      <span>{{ extraInfo.tzName }} (UTC{{ extraInfo.tzOffset }})</span>
       <span>Day {{ extraInfo.dayOfYear }}</span>
       <span>Week {{ extraInfo.week }}</span>
       <span>Q{{ extraInfo.quarterOfYear }}</span>
@@ -131,27 +126,13 @@ onMounted(() => {
     </small>
 
     <div id="toolbar" :class="{ hidden: isToolbarHidden }">
-      <span>
-        <code>F</code> Fullscreen
-      </span>
-      <span>
-        <code>T</code> Toggle toolbar
-      </span>
-      <span>
-        <code>D</code> Toggle dark mode
-      </span>
-      <span>
-        <code>H</code> Toggle 24-hour format
-      </span>
-      <span>
-        <code>I</code> Toggle extra info
-      </span>
+      <span><code>F</code> Fullscreen</span>
+      <span><code>T</code> Toggle toolbar</span>
+      <span><code>D</code> Toggle dark mode</span>
+      <span><code>H</code> Toggle 24-hour format</span>
+      <span><code>I</code> Toggle extra info</span>
     </div>
   </main>
-
-  <Modal title="Settings" :is-open="isSettingsOpen" @onClickClose="isSettingsOpen = false">
-    settings
-  </Modal>
 </template>
 
 <style scoped>
@@ -161,9 +142,9 @@ main {
 
   display: grid;
   place-content: center;
+
   background-color: var(--background);
 }
-
 
 #time-display {
   text-align: center;
