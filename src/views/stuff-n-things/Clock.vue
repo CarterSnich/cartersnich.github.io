0226408990c02753f63e4812f2075e16d0437a43
CarterSnich <jmeclosa@proton.me>
2025-11-25 12:15:02 +0800

    make toggle's states persistent

--- src/views/stuff-n-things/Clock.vue
+++ src/views/stuff-n-things/Clock.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { getTh } from '@/lib/utils';
-import { useDark, useToggle } from '@vueuse/core';
-import { computed, onMounted, ref } from 'vue';
+import { getTh } from "@/lib/utils";
+import { useDark, useStorage, useToggle } from "@vueuse/core";
+import { computed, onMounted, ref } from "vue";
 
 const FONTS = [
   "Anonymice Pro Nerd Font",
@@ -9,66 +9,79 @@ const FONTS = [
   "sans",
   "serif",
   "Nokia Cellphone FC",
-]
+];
 
 const toggleDark = useToggle(useDark());
 
-const isToolbarHidden = ref(false)
-const is24Hour = ref(true);
-const isExtraInfoHidden = ref(false)
-const fontIndex = ref(0)
+// const isToolbarHidden = ref(false)
+// const is24Hour = ref(true);
+// const isExtraInfoHidden = ref(false)
+// const fontIndex = ref(0)
 
-const now = ref(new Date())
+const settings = useStorage("clock-settings", {
+  istoolbarHidden: false,
+  is24Hour: false,
+  isExtraInfoHidden: false,
+  fontIndex: 0,
+});
+
+const now = ref(new Date());
 const timeDisplay = computed(() => {
   return now.value.toLocaleTimeString(undefined, {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-    hour12: is24Hour.value,
-  })
-})
+    hour12: settings.value.is24Hour,
+  });
+});
 const dateDisplay = computed(() => {
   return now.value.toLocaleDateString(undefined, {
     weekday: "long",
     year: "numeric",
     month: "long",
-    day: "numeric"
-  })
-})
+    day: "numeric",
+  });
+});
 
 const extraInfo = (() => {
   const d = new Date();
   const year = d.getFullYear();
-  const month = d.getMonth()
-  const date = d.getDate()
-  const time = d.getTime()
+  const month = d.getMonth();
+  const date = d.getDate();
+  const time = d.getTime();
 
   // timezone offset
   const m = d.getTimezoneOffset();
   const sign = m <= 0 ? "+" : "-";
-  const tzOffset = `${sign}${String(Math.floor(Math.abs(m) / 60)).padStart(2, "0")}:${String(Math.abs(m) % 60).padStart(2, "0")}`;
+  const tzOffset = `${sign}${String(Math.floor(Math.abs(m) / 60)).padStart(
+    2,
+    "0"
+  )}:${String(Math.abs(m) % 60).padStart(2, "0")}`;
 
   // timezone name
   const tzName = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   // day of year
-  const dayOfYear = Math.floor((time - new Date(year, 0, 0).getTime()) / 86400000);
+  const dayOfYear = Math.floor(
+    (time - new Date(year, 0, 0).getTime()) / 86400000
+  );
 
   // ISO week number
   const temp = new Date(year, month, date);
   const dayNum = (temp.getDay() + 6) % 7;
   temp.setDate(temp.getDate() - dayNum + 3);
   const week1 = new Date(temp.getFullYear(), 0, 4);
-  const week = 1 + Math.round(((temp.getTime() - week1.getTime()) / 86400000 - 3) / 7);
+  const week =
+    1 + Math.round(((temp.getTime() - week1.getTime()) / 86400000 - 3) / 7);
 
   // quarter of the year
-  const quarterOfYear = Math.floor((month + 3) / 3)
+  const quarterOfYear = Math.floor((month + 3) / 3);
 
   // leep year?
   const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 
   // get century
-  const century = Math.floor((year - 1) / 100) + 1
+  const century = Math.floor((year - 1) / 100) + 1;
 
   return {
     tzName,
@@ -77,47 +90,48 @@ const extraInfo = (() => {
     week,
     quarterOfYear,
     isLeapYear,
-    century
-  }
+    century,
+  };
 })();
 
 function onPressKeybinds(e: KeyboardEvent) {
   switch (e.key.toLowerCase()) {
     case "f":
       if (document.fullscreenEnabled) {
-        document.exitFullscreen()
+        document.exitFullscreen();
       }
       document.querySelector("main")?.requestFullscreen();
       break;
 
     case "t":
-      isToolbarHidden.value = !isToolbarHidden.value
+      settings.value.istoolbarHidden = !settings.value.istoolbarHidden;
       break;
 
     case "d":
-      toggleDark()
+      toggleDark();
       break;
 
     case "h":
-      is24Hour.value = !is24Hour.value
+      settings.value.is24Hour = !settings.value.is24Hour;
       break;
 
     case "i":
-      isExtraInfoHidden.value = !isExtraInfoHidden.value
+      settings.value.isExtraInfoHidden = !settings.value.isExtraInfoHidden;
       break;
 
     case "o":
-      if (fontIndex.value >= FONTS.length - 1) {
-        fontIndex.value = 0;
+      let i = settings.value.fontIndex;
+      if (i >= FONTS.length - 1) {
+        i = 0;
       } else {
-        fontIndex.value++;
+        i++;
       }
+      settings.value.fontIndex = i;
       break;
 
     default:
       // do supposed to do
       break;
-
   }
 }
 
@@ -125,24 +139,30 @@ onMounted(() => {
   setInterval(() => {
     now.value = new Date();
   }, 10);
-})
-
+});
 </script>
 
 <template>
   <main ref="main" @keydown="onPressKeybinds" tabindex="0" autofocus>
-    <div id="time-display" :style="{ 'font-family': FONTS[fontIndex] }">{{ timeDisplay }}</div>
+    <div
+      id="time-display"
+      :style="{ 'font-family': FONTS[settings.fontIndex] }">
+      {{ timeDisplay }}
+    </div>
     <div id="date-display">{{ dateDisplay }}</div>
-    <small id="extra-info" v-if="!isExtraInfoHidden">
+    <small id="extra-info" v-if="!settings.isExtraInfoHidden">
       <span>{{ extraInfo.tzName }} (UTC{{ extraInfo.tzOffset }})</span>
       <span>Day {{ extraInfo.dayOfYear }}</span>
       <span>Week {{ extraInfo.week }}</span>
       <span>Q{{ extraInfo.quarterOfYear }}</span>
       <span>{{ extraInfo.isLeapYear ? "Leap" : "Common" }} year</span>
-      <span>{{ extraInfo.century }}<sup>{{ getTh(extraInfo.century) }}</sup> Century</span>
+      <span
+        >{{ extraInfo.century
+        }}<sup>{{ getTh(extraInfo.century) }}</sup> Century</span
+      >
     </small>
 
-    <div id="toolbar" :class="{ hidden: isToolbarHidden }">
+    <div id="toolbar" :class="{ hidden: settings.istoolbarHidden }">
       <span><code>F</code> Fullscreen</span>
       <span><code>T</code> Toggle toolbar</span>
       <span><code>D</code> Toggle dark mode</span>
@@ -185,9 +205,8 @@ main {
   }
 
   span:last-child {
-    border: none
+    border: none;
   }
-
 }
 
 #toolbar {
@@ -195,8 +214,8 @@ main {
   left: 0;
   bottom: 0;
   width: 100%;
-  padding: .25rem .5rem;
-  opacity: .5;
+  padding: 0.25rem 0.5rem;
+  opacity: 0.5;
 
   display: flex;
   gap: 1rem;
@@ -207,6 +226,6 @@ main {
 }
 
 #toolbar.hidden:hover {
-  opacity: .5;
+  opacity: 0.5;
 }
 </style>
