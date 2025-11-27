a216cafa16698af1d2906037fcc16bb3c9d0c477
CarterSnich <jmeclosa@proton.me>
2025-11-27 21:29:10 +0800

    add portrait layout

--- src/views/stuff-n-things/Clock.vue
+++ src/views/stuff-n-things/Clock.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Modal from "@/components/Modal.vue";
+import useOrientation from "@/composable/useOrientation";
 import { getTh } from "@/lib/utils";
 import { useDark, useStorage, useToggle } from "@vueuse/core";
 import { computed, onMounted, ref } from "vue";
@@ -11,7 +13,10 @@ const FONTS = [
   "Nokia Cellphone FC",
 ];
 
+const isPortrait = useOrientation();
 const toggleDark = useToggle(useDark());
+
+const isSettingsVisible = ref(false);
 
 const settings = useStorage("clock-settings", {
   istoolbarHidden: false,
@@ -23,12 +28,23 @@ const settings = useStorage("clock-settings", {
 
 const now = ref(new Date());
 const timeDisplay = computed(() => {
-  return now.value.toLocaleTimeString(undefined, {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: settings.value.showSeconds ? "2-digit" : undefined,
-    hour12: settings.value.is24Hour,
-  });
+  const [a, b] = now.value
+    .toLocaleTimeString(undefined, {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: settings.value.showSeconds ? "2-digit" : undefined,
+      hour12: settings.value.is24Hour,
+    })
+    .split(" ");
+
+  const times = a?.split(":") as string[];
+
+  return {
+    hh: times[0] as string,
+    mm: times[1] as string,
+    ss: times[2],
+    M: b,
+  };
 });
 const dateDisplay = computed(() => {
   return now.value.toLocaleDateString(undefined, {
@@ -93,10 +109,7 @@ const extraInfo = (() => {
 function onPressKeybinds(e: KeyboardEvent) {
   switch (e.key.toLowerCase()) {
     case "f":
-      if (document.fullscreenEnabled) {
-        document.exitFullscreen();
-      }
-      document.querySelector("main")?.requestFullscreen();
+      switchFullscreen();
       break;
 
     case "t":
@@ -135,6 +148,13 @@ function onPressKeybinds(e: KeyboardEvent) {
   }
 }
 
+function switchFullscreen() {
+  if (document.fullscreenEnabled) {
+    document.exitFullscreen();
+  }
+  document.querySelector("main")?.requestFullscreen();
+}
+
 onMounted(() => {
   setInterval(() => {
     now.value = new Date();
@@ -143,34 +163,108 @@ onMounted(() => {
 </script>
 
 <template>
-  <main ref="main" @keydown="onPressKeybinds" tabindex="0" autofocus>
-    <div
-      id="time-display"
-      :style="{ 'font-family': FONTS[settings.fontIndex] }">
-      {{ timeDisplay }}
-    </div>
-    <div id="date-display">{{ dateDisplay }}</div>
-    <small id="extra-info" v-if="!settings.isExtraInfoHidden">
-      <span>{{ extraInfo.tzName }} (UTC{{ extraInfo.tzOffset }})</span>
-      <span>Day {{ extraInfo.dayOfYear }}</span>
-      <span>Week {{ extraInfo.week }}</span>
-      <span>Q{{ extraInfo.quarterOfYear }}</span>
-      <span>{{ extraInfo.isLeapYear ? "Leap" : "Common" }} year</span>
-      <span>
-        {{ extraInfo.century }}<sup>{{ getTh(extraInfo.century) }}</sup> Century
-      </span>
-    </small>
+  <template v-if="isPortrait">
+    <main
+      ref="main"
+      class="portrait"
+      tabindex="0"
+      autofocus
+      @click="isSettingsVisible = true"
+      @keydown="onPressKeybinds">
+      <div
+        id="time-display"
+        :style="{ 'font-family': FONTS[settings.fontIndex] }">
+        <div>
+          <div>{{ timeDisplay.hh }}</div>
+          <div>
+            {{ timeDisplay.mm }}
+          </div>
+        </div>
+        <div v-if="timeDisplay.ss || timeDisplay.M">
+          <span v-if="timeDisplay.ss">{{ timeDisplay.ss }}</span>
+          <span v-if="timeDisplay.M">{{ timeDisplay.M }}</span>
+        </div>
+      </div>
+      <div id="date-display">{{ dateDisplay }}</div>
+      <small id="extra-info" v-if="!settings.isExtraInfoHidden">
+        <span>{{ extraInfo.tzName }} (UTC{{ extraInfo.tzOffset }})</span>
+        <span>Day {{ extraInfo.dayOfYear }}</span>
+        <span>Week {{ extraInfo.week }}</span>
+        <span>Q{{ extraInfo.quarterOfYear }}</span>
+        <span>{{ extraInfo.isLeapYear ? "Leap" : "Common" }} year</span>
+        <span>
+          {{ extraInfo.century
+          }}<sup>{{ getTh(extraInfo.century) }}</sup> Century
+        </span>
+      </small>
+    </main>
 
-    <div id="toolbar" :class="{ hidden: settings.istoolbarHidden }">
-      <span><kbd>F</kbd> Fullscreen</span>
-      <span><kbd>T</kbd> Toggle toolbar</span>
-      <span><kbd>D</kbd> Toggle dark mode</span>
-      <span><kbd>H</kbd> Toggle 24-hour format</span>
-      <span><kbd>S</kbd> Toggle show seconds</span>
-      <span><kbd>I</kbd> Toggle extra info</span>
-      <span><kbd>O</kbd> Change font</span>
-    </div>
-  </main>
+    <Modal
+      title="Settings"
+      :is-open="isSettingsVisible"
+      @onClickClose="isSettingsVisible = false">
+      <div id="settings">
+        <div class="checkbox">
+          <input type="checkbox" id="is-24-hour" v-model="settings.is24Hour" />
+          <label for="is-24-hour">24-hour format</label>
+        </div>
+        <div class="checkbox">
+          <input
+            type="checkbox"
+            id="show-seconds"
+            v-model="settings.showSeconds" />
+          <label for="show-seconds">Show seconds</label>
+        </div>
+        <div class="checkbox">
+          <input
+            type="checkbox"
+            id="hide-extra-info"
+            v-model="settings.isExtraInfoHidden" />
+          <label for="hide-extra-info">Hide extra info</label>
+        </div>
+        <select id="font-index" v-model="settings.fontIndex">
+          <option v-for="(f, index) in FONTS" :value="index">{{ f }}</option>
+        </select>
+        <button for="fullscreen" @click="switchFullscreen">
+          Go fullscreen
+        </button>
+      </div>
+    </Modal>
+  </template>
+  <template v-else>
+    <main ref="main" @keydown="onPressKeybinds" tabindex="0" autofocus>
+      <div
+        id="time-display"
+        :style="{ 'font-family': FONTS[settings.fontIndex] }">
+        <span>{{ timeDisplay.hh }}</span>
+        <span>{{ `:${timeDisplay.mm}` }}</span>
+        <span v-if="timeDisplay.ss">{{ `:${timeDisplay.ss}` }}</span>
+        <span v-if="timeDisplay.M">{{ ` ${timeDisplay.M}` }}</span>
+      </div>
+      <div id="date-display">{{ dateDisplay }}</div>
+      <small id="extra-info" v-if="!settings.isExtraInfoHidden">
+        <span>{{ extraInfo.tzName }} (UTC{{ extraInfo.tzOffset }})</span>
+        <span>Day {{ extraInfo.dayOfYear }}</span>
+        <span>Week {{ extraInfo.week }}</span>
+        <span>Q{{ extraInfo.quarterOfYear }}</span>
+        <span>{{ extraInfo.isLeapYear ? "Leap" : "Common" }} year</span>
+        <span>
+          {{ extraInfo.century
+          }}<sup>{{ getTh(extraInfo.century) }}</sup> Century
+        </span>
+      </small>
+
+      <div id="toolbar" :class="{ hidden: settings.istoolbarHidden }">
+        <span><kbd>F</kbd> Fullscreen</span>
+        <span><kbd>T</kbd> Toggle toolbar</span>
+        <span><kbd>D</kbd> Toggle dark mode</span>
+        <span><kbd>H</kbd> Toggle 24-hour format</span>
+        <span><kbd>S</kbd> Toggle show seconds</span>
+        <span><kbd>I</kbd> Toggle extra info</span>
+        <span><kbd>O</kbd> Change font</span>
+      </div>
+    </main>
+  </template>
 </template>
 
 <style scoped>
@@ -183,15 +277,35 @@ main {
 
   background-color: var(--background);
 }
+main.portrait {
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 5rem;
+}
 
 #time-display {
   text-align: center;
   font-size: 10rem;
 }
+.portrait > #time-display {
+  display: flex;
+  line-height: 1;
+  margin: auto;
+
+  > div:nth-child(2) {
+    padding-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    font-size: 4rem;
+  }
+}
 
 #date-display {
   text-align: center;
   font-size: 3rem;
+}
+.portrait > #date-display {
+  font-size: 2rem;
 }
 
 #extra-info {
